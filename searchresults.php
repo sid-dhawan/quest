de9c25afafbe27a2d2d524d49dca2c6d60c40e56
v98 <vrinda16279@iiitd.ac.in>
2016-11-18 20:54:03 +0530

    Add files via upload

---
+++ searchresults.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html>
+<head>
+	
+	<meta charset="UTF-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge"">
+	<meta name="viewport" content="width = device-width, initial-scale = 1">
+	<title>Search Results</title>
+	<link rel="stylesheet" type="text/css" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+	<style>
+	.jumbotron a{
+		font-family: "Comic Sans MS";
+		font-size: 150%;
+		color: blue;
+	}
+	.jumbotron #link{
+		font-family:Tahoma, Geneva, sans-serif;
+		color: green;
+	}
+	
+	</style>
+</head>
+<body>
+<div class="container">
+<div class="row">
+<div class="col-lg-5 col-md-7 col-sm-9 col-xs-12">
+<div class="page-header">
+<h1>Search Results:</h1>
+</div>
+<div class="jumbotron">
+<!--can have all search results coming here-->
+<!--eg of one search element-->
+<a href="https://www.youtube.com/watch?v=gqOEoUR5RHg">Bootstrap Tutorial</a>
+<div id="link"><p>https://www.youtube.com/watch?v=gqOEoUR5RHg</p></div>
+<!--end of 1 search element-->
+</div>
+<p>
+<a href="http://sid-dhawan.eu5.org/Quest/ex3.php" class="btn btn-lg btn-primary" role="button">New Search</a>
+</p>
+</div>
+</div>
+
+
+
+</div>
+
+
+
+
+
+
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
+</body>
+</html>
